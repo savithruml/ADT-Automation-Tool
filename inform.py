c66fbe809e78159b901f148693c99b1a4702ad63
Savvy_Through <savithruml@users.noreply.github.com>
2016-02-11 13:12:39 -0700

    Update inform.py

--- inform.py
+++ inform.py
@@ -8,13 +8,13 @@ from email.MIMEText import MIMEText
 def send_mail(mail_id, file_download):
 	
 	os.chdir('/home-backup/ADT/adt_tools')
-	sender_mail = 'itzetaboulder@gmail.com'
+	sender_mail = ''
 	receiver_mail = mail_id
 	mesg = '\nBelow are the scrubbed files\n'
     
 	try:
 		msg = MIMEMultipart()
-		msg['From'] = 'ITZetaBoulder@gmail.com'
+		msg['From'] = ''
 		msg['To'] = 'E-Mailers'
 		msg['Subject'] = 'IMPORTANT: ADT Scrub Process Completed'
 		msg.attach(MIMEText(mesg, 'plain'))
@@ -23,7 +23,7 @@ def send_mail(mail_id, file_download):
 		smtp_session.ehlo()
 		smtp_session.starttls()
 		smtp_session.ehlo()
-		smtp_session.login(sender_mail, 'ZetaAdminMe123')
+		smtp_session.login(sender_mail, '')
 		smtp_session.sendmail(sender_mail, receiver_mail, msg.as_string())
 		smtp_session.quit()
 		print '\nDone Processing! Access files here: /opt/admin_files/data/'
@@ -35,26 +35,26 @@ def send_mail(mail_id, file_download):
 	
 def message_Twilio(to_number):
 
-    sid = "AC4e45219ecfc768c5548964cf69f1118d"
-    token = "591a88cc435bbe2ac192f938ac03d534"
+    sid = ""
+    token = ""
 
     client = TwilioRestClient(sid, token)
     body = '\nADT FILE SCRUB COMPLETED'
     
-    client.messages.create(to = to_number, from_= '+17206132910',
+    client.messages.create(to = to_number, from_= '',
                             body=body,)
 
 def call_Twilio(to_number):
 	
-	sid = "AC4e45219ecfc768c5548964cf69f1118d"
-	token = "591a88cc435bbe2ac192f938ac03d534"
+	sid = ""
+	token = ""
 
 	client = TwilioRestClient(sid, token)
 	
 	url_msg = 'http://twimlets.com/message?Message%5B0%5D=Attention%20E-mailers%20this%20is%20an%20automated%20message%20from%20the%20IT%20department.%20The%20ADT%20Scrub%20Process%20has%20been%20Completed&'
 	#'http://twimlets.com/message?Message%5B0%5D=Attention%20E-mailers%2C%20ADT%20Scrub%20Process%20has%20been%20Completed&'
 
-	call = client.calls.create(to='+17208768596', from_='+17206132910',
+	call = client.calls.create(to='', from_='',
 			url=url_msg, method='GET', fallback_method='GET',
 			status_callback_method='GET', record='false')
 
